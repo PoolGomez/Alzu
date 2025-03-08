16205ecc6a0333a58adb93aec855d543d57cdacb
unknown <pgomez4790@gmail.com>
2025-03-08 14:56:06 -0500

    fix fix

--- utils/hash-password.ts
+++ utils/hash-password.ts
@@ -1,6 +1,5 @@
 export const runtime = "nodejs";
 import bcrypt from "bcryptjs";
 export const encrypt= async(text: string)=>{
-    const saltRounds = 10; // Número de rondas de sal
-    return await bcrypt.hash(text, saltRounds);
+    return await bcrypt.hash(text, 10);
 }
