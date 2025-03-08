3bf5790eb9edcdbd6b670717f950cee80cc8ab32
unknown <pgomez4790@gmail.com>
2025-03-08 11:45:54 -0500

    chage bcryptjs for bcrypt

--- auth.config.ts
+++ auth.config.ts
@@ -4,7 +4,7 @@ import type { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import { loginSchema } from "./lib/zod";
 import { db } from "./lib/db";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import {nanoid} from"nanoid"
 import { sendEmailVerification } from "./lib/mail";
 
--- utils/hash-password.ts
+++ utils/hash-password.ts
@@ -1,5 +1,5 @@
 export const runtime = "nodejs";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 export const encrypt= async(text: string)=>{
     const saltRounds = 10; // Número de rondas de sal
     return await bcrypt.hash(text, saltRounds);
